2d0c2bb016155fb2816ec8a67cfe1ccd777bdbdb
E27310 <robert.stelling@sri.com>
2022-12-28 17:44:13 -0500

    Old WinForms program from StackOverflow to get color RGB and color itself to color under the pixel

---
+++ PixelColors/PixelColors/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PixelColors
+{
+    // from https://stackoverflow.com/questions/1483928/how-to-read-the-color-of-a-screen-pixel
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+    
+        }
+
+
+        private void MouseMoveTimer_Tick(object sender, EventArgs e)
+        {
+            Point cursor = new Point();
+            GetCursorPos(ref cursor);
+
+            var c = GetColorAt(cursor);
+            this.BackColor = c;
+
+            textBox1.Text = String.Format("{0} {1} {2} at {3}, {4}", c.R, c.G, c.B, cursor.X, cursor.Y);
+             
+            if (c.R == c.G && c.G < 64 && c.B > 128)
+            {
+                // Got several dozen of these cacscading 
+               // MessageBox.Show("Blue");
+            }
+        }
+
+        Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+        public Color GetColorAt(Point location)
+        {
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr hSrcDC = gsrc.GetHdc();
+                    IntPtr hDC = gdest.GetHdc();
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+
+            return screenPixel.GetPixel(0, 0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MouseMoveTimer_Tick(sender, e);
+        }
+
+
+    }
+}
